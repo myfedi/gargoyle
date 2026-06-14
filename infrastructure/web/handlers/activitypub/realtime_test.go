99e755475f304215163fd23e84028ae9094118d8
Christian Schulze <accounts@andinfinity.de>
2026-06-14 16:38:42 +0200

    Test inbox accept realtime publishing

--- infrastructure/web/handlers/activitypub/realtime_test.go
+++ infrastructure/web/handlers/activitypub/realtime_test.go
@@ -1,8 +1,12 @@
 package activitypub
 
 import (
+	"net/http/httptest"
+	"strings"
 	"testing"
 
+	"github.com/gofiber/fiber/v2"
+	"github.com/myfedi/gargoyle/domain/models"
 	apUsecases "github.com/myfedi/gargoyle/domain/usecases/activitypub"
 )
 
@@ -30,5 +34,38 @@ func TestPublishRealtimeSkipsMissingClientUpdate(t *testing.T) {
 
 	if called {
 		t.Fatal("publisher should not be called without ClientUpdate")
+	}
+}
+
+func TestInboxAcceptPublishesRelationshipUpdate(t *testing.T) {
+	app := fiber.New()
+	remoteActor := "https://remote.example/users/bob"
+	follows := &fakeFollowsRepo{followers: []models.Follow{{ID: "following", LocalAccountID: "account-1", RemoteActor: remoteActor, Direction: "following"}}}
+	activities := &fakeActivitiesRepo{}
+	var gotLocal, gotActor string
+	var gotNotifications bool
+	handler := newTestHandler(fakeAccountsRepo{}, activities, follows)
+	handler.SetRealtimePublisher(func(localAccountID, remoteActor string, notifications bool) {
+		gotLocal = localAccountID
+		gotActor = remoteActor
+		gotNotifications = notifications
+	})
+	handler.SetupRoutes(app)
+
+	body := `{"type":"Accept","actor":"https://remote.example/users/bob","object":{"type":"Follow","actor":"https://example.org/users/alice","object":"https://remote.example/users/bob"}}`
+	resp, err := app.Test(httptest.NewRequest("POST", "/users/alice/inbox", strings.NewReader(body)))
+	if err != nil {
+		t.Fatalf("request failed: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != fiber.StatusAccepted {
+		t.Fatalf("expected 202, got %d", resp.StatusCode)
+	}
+	if gotLocal != "account-1" || gotActor != remoteActor {
+		t.Fatalf("unexpected realtime publish local=%q actor=%q", gotLocal, gotActor)
+	}
+	if gotNotifications {
+		t.Fatal("Accept should publish relationship update without notification update")
 	}
 }
